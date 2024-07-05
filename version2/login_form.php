0852b3d198b6c04a4eca14faaed27b5b84b2692f
R-Asiedua <rnaatenkorang@gmail.com>
2024-07-05 22:24:57 +0000

    version2

--- version2/login_form.php
+++ version2/login_form.php
@@ -1,12 +1,17 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   $username = $_POST['username'];
-   $password = $_POST['password'];
-   // Process the login here ;enter the if conditions
-   if ($username == "Nana Adjoa" && $password == "Regina123" ) {
-        echo "Welcome,This is admin!";
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    // Defining the desired username and password
+    $desired_username = "Adjoa"; 
+    $desired_password = "Regina123"; 
+
+    // Check if the inputted username and password are correct
+    if ($username === $desired_username && $password === $desired_password) {
+        echo "Welcome, This is admin! " . $username . "!";
     } else {
-        echo "Invalid username or password";
-    }  
+        echo "Invalid username or password.";
+    }
 }
 ?>
